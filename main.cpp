690f12c05748d65191b34c2903825cf562108d44
Taylor Griffin <taylor.griffin.365@gmail.com>
2020-10-14 11:12:05 -0700

    Added commandline styling options

--- main.cpp
+++ main.cpp
@@ -6,11 +6,57 @@
 extern int yylex();
 extern Node *root; 
 
-int main() {
+int main(int argc, char** argv) {
+  // set default colorscheme and direction
+  bool vertical = true;
+  bool dark_mode = false;
+
   // fetch symbols from parser with yylex
   if (!yylex()) {
+
+    // check for --color and --dir commandline options
+    if (argc > 2) {
+      if (argv[1] == std::string("--color") && argv[2] == std::string("dark")) {
+        dark_mode = true;
+      }
+      else if (argv[1] == std::string("--dir") && argv[2] == std::string("horizontal")) {
+        vertical = false;
+      }
+    }
+    if (argc > 4) {
+      if (argv[3] == std::string("--color") && argv[4] == std::string("dark")) {
+        dark_mode = true;
+      }
+      else if (argv[3] == std::string("--dir") && argv[4] == std::string("horizontal")) {
+        vertical = false;
+      }
+    }
+
     // print beginning of graphviz spec
     std::cout << "digraph G {" << std::endl;
+
+    // define graph styling
+    std::cout << "  graph [center=1";
+    if (!vertical) {
+      std::cout << " rankdir=LR";
+    }
+    if (dark_mode) {
+      std::cout << " bgcolor=\"#121212\"";
+    }
+    std::cout << "]" << std::endl;
+
+    // define node styling
+    std::cout << "  node [fontname=\"Courier\"";
+    if (dark_mode) {
+      std::cout << " color=\"#FFFFFF\" style=\"filled\"";
+    }
+    std::cout << "]" << std::endl;
+
+    // define edge styling
+    if (dark_mode) {
+      std::cout << "  edge [color=\"#FFFFFF\"]";
+    }
+
     // print nodes recursively
     print_node(root, "n0", 0);
     // print end of graphviz spec
